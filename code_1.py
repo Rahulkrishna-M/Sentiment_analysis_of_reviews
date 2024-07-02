fa4545cfdf8ff3a8e8b5de3d4586b24233dc7327
Rahul Krishna <102946334+Rahulkrishna-M@users.noreply.github.com>
2024-07-02 19:51:35 +0530

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import *
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report, confusion_matrix
 import joblib
-# Function to parse the gzip file and create DataFrame
+
 def parse_gzip_to_df(path):
     def parse(path):
         g = gzip.open(path, 'rb')
@@ -28,41 +28,37 @@ def label_review(overall):
     else:
         return 'negative'
 
-# Path to the gzip JSON file
-file_path = r"C:\Users\ASUS\Desktop\FUN SIMPLY\polynomial\Cell_Phones_and_Accessories_5.json.gz"
+file_path = r"C:\Users\ASUS\Desktop\polynomial\Cell_Phones_and_Accessories_5.json.gz"
 
-# Create DataFrame
+
 reviews = parse_gzip_to_df(file_path)
-reviews.dropna(subset=['summary'], inplace=True)  # Handle missing values in 'reviewText' column
+reviews.dropna(subset=['summary'], inplace=True)  
 reviews['review_label'] = reviews['overall'].apply(label_review)
 
-# Split the data
 X_train, X_test, y_train, y_test = train_test_split(reviews['summary'], reviews['review_label'], test_size=0.2, random_state=42)
 
-# Create a pipeline with TF-IDF and Logistic Regression
 pipeline = Pipeline([
     ('tfidf', TfidfVectorizer(max_features=10000)),
     ('clf', LogisticRegression(solver='liblinear'))
 ])
 
-# Train the model
+# Training the model
 pipeline.fit(X_train, y_train)
 
-# Predict on test data
+# Predicting on test data
 y_pred = pipeline.predict(X_test)
 
-# Generate metrics
+
 conf_matrix = confusion_matrix(y_test, y_pred)
 class_report = classification_report(y_test, y_pred)
 
-# Output results
 print("Confusion Matrix:")
 print(conf_matrix)
 print("\nClassification Report:")
 print(class_report)
 
-# Save the model to disk
-model_file_path = r'C:\Users\ASUS\Desktop\FUN SIMPLY\polynomial\Cell_Phones_and_Accessories_5.json\sentiment_analysis_model.pkl'
+
+model_file_path = r'C:\Users\ASUS\Desktop\polynomial\Cell_Phones_and_Accessories_5.json\sentiment_analysis_model.pkl'
 joblib.dump(pipeline, model_file_path)
 print(f"Model saved to {model_file_path}")
 
@@ -70,8 +66,5 @@ def predict_sentiment(text):
     prediction = pipeline.predict([text])
     return prediction[0]
 
-# # Example usage
-# input_text = "It is not great at all "
-# predicted_sentiment = predict_sentiment(input_text)
-# print("\nThe predicted sentiment of the review is:", predicted_sentiment)
+
 
